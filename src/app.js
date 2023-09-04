0225616545c89e31ebd0d988fb84260f61cacba5
sandrorzimmer <sandrorzimmer@gmail.com>
2023-09-04 19:50:42 -0300

    Implemented CORS configuration

--- src/app.js
+++ src/app.js
@@ -1,9 +1,18 @@
 import express from 'express';
+import cors from 'cors';
 import routes from './routes/index.js';
 import handler404 from './middlewares/handler404.js';
 import errorHandler from './middlewares/errorHandler.js';
 
+const corsOptions = {
+    origin: 'https://example.com', // replace with the real origin
+    methods: 'GET, HEAD, PUT, POST, DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204,
+};
+
 const app = express();
+app.use(cors(corsOptions));
 app.use(express.json());
 routes(app);
 app.use(handler404);
